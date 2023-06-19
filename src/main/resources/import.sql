784a35a756dee524556b4c8047dc41b785355898
13defebrerodel98 <manuelvargasa13@gmail.com>
2023-06-19 11:33:38 -0500

    primera version

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO productos (nombre, precio, create_at) VALUES('Panasonic', 800, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Panasonic2', 802, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Panasonic3', 803, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Panasonic4', 804, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Panasonic5', 805, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Panasonic6', 806, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Panasonic7', 807, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Panasonic8', 808, NOW());
